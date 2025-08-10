4c3099c63d2f04780f0ed0160aebf8dd9baa86d9
HenriqueVieira03 <henrivira14@outlook.com>
2025-08-10 14:32:55 -0300

    Criação de Métodos
    - Criado Métodos para criar, concluir/reabrir (editar) e excluir uma tarefa.

--- src/main/java/br/com/portfolio/gerenciadortarefas/controller/TarefaController.java
+++ src/main/java/br/com/portfolio/gerenciadortarefas/controller/TarefaController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class TarefaController {
@@ -17,11 +21,43 @@ public class TarefaController {
 
     @GetMapping("/tarefas")
     public String listarTarefas(Model model) {
-
         List<Tarefa> tarefas = tarefaRepository.findAll();
 
         model.addAttribute("tarefas", tarefas);
 
         return "tarefas-lista";
+    }
+
+    @GetMapping("/tarefas/nova")
+    public String mostrarFormularioDeNovaTarefa(Model model) {
+        model.addAttribute("tarefa", new Tarefa());
+
+        return "tarefa-form";
+    }
+
+    @PostMapping("/tarefas")
+    public String salvarTarefa(@ModelAttribute("tarefa") Tarefa tarefa) {
+        tarefaRepository.save(tarefa);
+
+        return "redirect:/tarefas";
+    }
+
+    @PostMapping("/tarefas/concluir/{id}")
+    public String concluirTarefa(@PathVariable("id") Long id) {
+        Optional<Tarefa> tarefaOpt = tarefaRepository.findById(id);
+
+        if (tarefaOpt.isPresent()) {
+            Tarefa tarefa = tarefaOpt.get();
+            tarefa.setConcluida(!tarefa.isConcluida());
+            tarefaRepository.save(tarefa);
+        }
+
+        return "redirect:/tarefas";
+    }
+    @PostMapping("/tarefas/excluir/{id}")
+    public String excluirTarefa(@PathVariable("id") Long id) {
+        tarefaRepository.deleteById(id);
+
+        return "redirect:/tarefas";
     }
 }
